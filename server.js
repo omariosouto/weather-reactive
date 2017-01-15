babd56f2233cb0174599c0bc401be4aff99d5c73
Mario Souto <Mario Souto>
2017-01-15 03:56:42 -0200

    fix

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
+var express = require('express');
 
 // Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(function(req, res, next){
-// 	if(req.headers['x-forwarded-proto'] === 'http') {
-// 		next();
-// 	} else {
-// 		req.redirect('http://' + req.hostname + req.url);
-// 	}
-// });
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
